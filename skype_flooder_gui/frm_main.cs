4552f3a3cf5ae8d1fc2b56967702089e96582972
kasthack <kasthack@epicm.org>
2013-05-19 02:39:28 +0400

    engine

--- skype_flooder_gui/frm_main.cs
+++ skype_flooder_gui/frm_main.cs
@@ -2,80 +2,78 @@
 using System.Windows.Forms;
 using SKYPE4COMLib;
 using System.Text.RegularExpressions;
-
-namespace skype_flooder_gui
-{
-    public partial class frm_main : Form
-    {
-        private static readonly frm_main TheInstance = new frm_main(); 
-        Skype skype = new SkypeClass();
-         
-        public frm_main()
-        {
-          InitializeComponent(); 
-          int protocol = Convert.ToInt32(skype_protokol.Value);  //skype rotocol fix (dem to int)
-          skype.Attach(protocol, false);  //skype.Attach(8, false);
-        }
-
-        private void frm_main_Load(object sender, EventArgs e)
-        {
-             
-            try
-            {
-                Regex r = new Regex(@"xmpp\:.*");
-                foreach (User friend in skype.Friends)
-                {
-                    if (!r.IsMatch(friend.Handle))
-                        list_users.Items.Add(new skype_friends() { s_user = friend });
-                }
-            }
-            catch
-            {
-                frm_warning frmwarning = new frm_warning();
-                frmwarning.ShowDialog();
-            }
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            if (rb_single_sending.Checked)
-            {
-                skype.SendMessage(txt_target.Text, txt_flood_text.Text);
-                //beep
-                txt_flood_text.Text = "";
-            }
-        }
-
-        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            txt_target.Text = ((skype_friends)list_users.SelectedItem).s_user.Handle;
-        }
-
-        private void chk_status_flooding_CheckedChanged(object sender, EventArgs e)
-        {
-            rb_fuckoff.Enabled = chk_status_flooding.Checked;
-            rb_ghost.Enabled = chk_status_flooding.Checked;
-            rb_nothere.Enabled = chk_status_flooding.Checked;
-            rb_online.Enabled = chk_status_flooding.Checked;
-        }
-
-        private void txt_flood_text_TextChanged(object sender, EventArgs e)
-        {
-            if (txt_flood_text.Text == "" & rb_single_sending.Checked)
-            {
-                btn_flooding.Enabled = false;
-            }
-            else
-            {
-                btn_flooding.Enabled = true;
-            }
-        }
-
-        private void brn_about_Click(object sender, EventArgs e)
-        {
-            frm_about frmabout = new frm_about();
-            frmabout.ShowDialog();
-        }
-   
-    }
+using System.Threading;
+namespace skype_flooder_gui {
+	public partial class frm_main : Form {
+		Skype skype = new SkypeClass();
+		Thread thr;
+		bool skype_attached = false;
+		private bool _attacking;
+		public bool Attacking {
+			get {
+				return _attacking;
+			}
+			set {
+				if ( value & !_attacking && !thr.IsAlive)
+					this.thr.Start();
+				_attacking = value;
+				btn_flooding.Text = !value ? "Stop" : "Flood!";
+			}
+		}
+		
+		public frm_main() {
+			//int protocol = Convert.ToInt32( skype_protokol.Value );
+			//skype.Attach( protocol, false );
+			this.thr=new Thread( new ThreadStart( AttackProcess ) );
+			InitializeComponent();
+		}
+		private void on_load( object sender, EventArgs e ) {
+			try {
+				Regex r = new Regex( @"xmpp\:.*" );
+				foreach ( User friend in skype.Friends ) {
+					if ( !r.IsMatch( friend.Handle ) )
+						list_users.Items.Add( new skype_friends() {
+							s_user = friend
+						} );
+				}
+			}
+			catch {
+				new frm_warning().ShowDialog();
+			}
+		}
+		private void run_stop_attack( object sender, EventArgs e ) {
+			this.Attacking ^= true;
+		}
+		void attach_skype() {
+			if ( !this.skype_attached ) {
+				skype.Attach( Convert.ToInt32( skype_protokol.Value ), false );
+				skype_attached = true;
+			}
+		}
+		private void AttackProcess(){
+			string target="", message="";
+			int delay = 0, flood_type = 0;
+			this.Invoke((Action)(()=>{
+				target = this.txt_target.Text;
+				message = txt_flood_text.Text;
+				delay = Convert.ToInt32( this.nud_delay.Value );
+				flood_type = this.rb_infinite_messages.Checked ? 0 : this.rb_timeout_messages.Checked ? 1 : 2;
+			}));
+			attach_skype();
+			if ( flood_type == 2 ) {
+				try {
+					this.skype.SendMessage( target, message );
+					this.Invoke( (Action)( () => Attacking = false ) );
+				}
+				catch {
+				}
+				return;
+			}
+			while ( this.Attacking ) {
+				this.skype.SendMessage( target, message );
+				if ( flood_type == 1 )
+					Thread.Sleep( delay );
+			}
+		}
+	}
 }
